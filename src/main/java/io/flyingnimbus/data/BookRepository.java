14535d992e4a17a28ae3f1a1567b18432f4aaa8b
Kye Yeung <yeung.kye@gmail.com>
2018-07-27 11:37:51 +0100

    screenshots with partial to full non-blocking migration

--- src/main/java/io/flyingnimbus/data/BookRepository.java
+++ src/main/java/io/flyingnimbus/data/BookRepository.java
@@ -3,14 +3,8 @@ package io.flyingnimbus.data;
 import io.flyingnimbus.domain.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 /**
  * @author Kye
  */
 public interface BookRepository extends MongoRepository<Book, String> {
-
-    Book findByIsbn(String isbn);
-
-    List<Book> findByAuthor(String author);
 }
